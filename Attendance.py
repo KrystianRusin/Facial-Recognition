9a456712212bdc9df077fb918a3a9891e4c9b7f5
KrystianRusin <36743674+KrystianRusin@users.noreply.github.com>
2021-03-28 16:29:31 -0400

    Update Attendance.py

--- Attendance.py
+++ Attendance.py
@@ -10,11 +10,18 @@ while True:
 
     grayScale = cv2.cvtColor(camera, cv2.COLOR_BGR2GRAY)
 
-    faces = faceCascade.detectMultiScale(grayScale, scaleFactor=1.5, minNeighbors=5, minSize=(30, 30),
-                                         flags=cv2.CASCADE_SCALE_IMAGE)
+    faces = faceCascade.detectMultiScale(
+        grayScale,
+        scaleFactor=1.5,
+        minNeighbors=5,
+        minSize=(30, 30),
+        flags=cv2.CASCADE_SCALE_IMAGE
+    )
 
     for (x, y, w, h) in faces:
         cv2.rectangle(camera, (x, y), (x+w, y+h), (153,50,204), 2)
+
+    cv2.imshow('Video', camera)
 
     if cv2.waitKey(1) & 0xFF == ord('q'):
         break
